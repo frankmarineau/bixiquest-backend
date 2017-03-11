72bd93620a26e3b8106ac3f9ed2d92cc1c2c9796
DrPandemic <bipbip500@gmail.com>
2017-03-11 14:59:44 -0500

    Adds seed

--- migrations/20170311122941_add_bixi_stations.js
+++ migrations/20170311122941_add_bixi_stations.js
@@ -3,7 +3,7 @@ exports.up = (knex, Promise) =>
   knex.schema.createTable('bixi_stations', (t) => {
     t.bigInteger('id').primary()
     t.string('name')
-    t.specificType('pos', 'geometry(point, 4326)')
+    t.specificType('pos', 'geometry(point, 0)')
   })
 
 exports.down = (knex, Promise) =>
